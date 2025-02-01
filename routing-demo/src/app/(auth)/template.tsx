81a0b99694def953bbfe15fbb249286191596d80
Sasikant Biswal <biswalsashikant@gmail.com>
2025-02-01 16:52:59 +0530

    templates

--- routing-demo/src/app/(auth)/template.tsx
+++ routing-demo/src/app/(auth)/template.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 import "./styles.css";
 
 const navLinks = [
@@ -15,14 +16,27 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+  const [input, setInput] = useState("");
 
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
       {navLinks.map((link) => {
         const isActive =
-          pathname === link.href || pathname.startsWith(link.href) && link.href !== "/";
+          pathname === link.href ||
+          (pathname.startsWith(link.href) && link.href !== "/");
         return (
-          <Link className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"} href={link.href} key={link.name}>
+          <Link
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+            href={link.href}
+            key={link.name}
+          >
             {link.name}
           </Link>
         );
