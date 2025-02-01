ba5d158a53a645a23a41eb85588848cb3dbe6c91
Sasikant Biswal <biswalsashikant@gmail.com>
2025-02-01 17:13:31 +0530

    error.tsx (Error Boundary)

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,18 @@
 import { notFound, redirect } from "next/navigation";
 
+function getRandomInit(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 const ProductReview = async ({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) => {
+  const random = getRandomInit(2);
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
   const { productId, reviewId } = await params;
 
   if (parseInt(reviewId) > 1000) {
