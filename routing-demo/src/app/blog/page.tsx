a23932f7cc13b0cd37abed67c725fc7bf44fa745
Sasikant Biswal <biswalsashikant@gmail.com>
2025-02-01 17:01:25 +0530

    Loading.tsx

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -2,11 +2,16 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    absolute: "Blog"
+    absolute: "Blog",
   },
 };
 
-const Blog = () => {
+const Blog = async () => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Intentional delay");
+    }, 2000);
+  });
   return <h1>My blog</h1>;
 };
 
