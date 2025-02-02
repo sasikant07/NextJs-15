ca53eb3faea6df975e4932a0a7608b6a33780e2f
Sasikant Biswal <biswalsashikant@gmail.com>
2025-02-03 00:08:28 +0530

    GET, POST, PATCH, DELETE Route handlers

---
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -0,0 +1,38 @@
+import { comments } from "../data";
+
+export async function GET(
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const comment = comments.find((cmnt) => cmnt.id === parseInt(id));
+  return Response.json(comment);
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const body = await request.json();
+  const { text } = body;
+
+  const index = comments.findIndex((cmnt) => cmnt.id === parseInt(id));
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
+}
+
+export async function DELETE(
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+
+  const index = comments.findIndex((cmnt) => cmnt.id === parseInt(id));
+  const deletedComment = comments[index];
+
+  comments.splice(index, 1);
+
+  return Response.json(deletedComment);
+}
