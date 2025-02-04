baa72b154eef074e4460fa3a463e55ea6c4f21aa
Sasikant Biswal <biswalsashikant@gmail.com>
2025-02-05 03:25:21 +0530

    Delete server action

--- data-fetching-demo/src/actions/products.ts
+++ data-fetching-demo/src/actions/products.ts
@@ -41,29 +41,38 @@ export async function createProduct(prevState: FormState, formData: FormData) {
   redirect("/products-db");
 }
 
-export async function editProduct(id: number, prevState: FormState, formData: FormData) {
-    const title = formData.get("title") as string;
-    const price = formData.get("price") as string;
-    const description = formData.get("description") as string;
-  
-    const errors: Errors = {};
-  
-    if (!title) {
-      errors.title = "Title is required";
-    }
-  
-    if (!price) {
-      errors.price = "Price is required";
-    }
-  
-    if (!description) {
-      errors.description = "Description is required";
-    }
-  
-    if (Object.keys(errors).length > 0) {
-      return { errors };
-    }
-  
-    await updateProduct(id, title, parseInt(price), description);
-    redirect("/products-db");
+export async function editProduct(
+  id: number,
+  prevState: FormState,
+  formData: FormData
+) {
+  const title = formData.get("title") as string;
+  const price = formData.get("price") as string;
+  const description = formData.get("description") as string;
+
+  const errors: Errors = {};
+
+  if (!title) {
+    errors.title = "Title is required";
   }
+
+  if (!price) {
+    errors.price = "Price is required";
+  }
+
+  if (!description) {
+    errors.description = "Description is required";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  await updateProduct(id, title, parseInt(price), description);
+  redirect("/products-db");
+}
+
+export async function removeProduct(id: number) {
+  await deleteProduct(id);
+  revalidatePath("/products-db");
+}
