3ac2ae227775b75cca207e61d59ede2c686d0436
Sasikant Biswal <biswalsashikant@gmail.com>
2025-02-05 01:54:54 +0530

    From Validation with useActionState

--- data-fetching-demo/src/app/products-db-create/page.tsx
+++ data-fetching-demo/src/app/products-db-create/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import { Submit } from "@/components/submit";
 import { addProduct } from "@/prisma-db";
 import { redirect } from "next/navigation";
+import { useActionState } from "react";
+
+type Errors = {
+    title: string;
+    price: string;
+    description: string;
+  };
+  
+  type FormState = {
+    errors: Errors;
+  };
 
 export default function AddProductPage() {
+  const initialState: FormState = {
+    errors: {},
+  };
+
+  const [state, formAction, isPending] = useActionState(createProduct, initialState);
   async function createProduct(formData: FormData) {
     "use server";
 
@@ -10,12 +28,28 @@ export default function AddProductPage() {
     const price = formData.get("price") as string;
     const description = formData.get("description") as string;
 
+    const errors: Errors = {};
+
+    if (!title) {
+      errors.title = "Title is required";
+    }
+    if (!price) {
+      errors.title = "Price is required";
+    }
+    if (!description) {
+      errors.title = "Description is required";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      return { errors };
+    }
+
     await addProduct(title, parseInt(price), description);
     redirect("/products-db");
   }
 
   return (
-    <form action={createProduct} className="p-4 space-y-4 max-w-96">
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
       <div>
         <label className="text-white">
           Title
@@ -25,6 +59,9 @@ export default function AddProductPage() {
             name="title"
           />
         </label>
+        {state.errors.title && (
+          <p className="text-red-500">{state.errors.title}</p>
+        )}
       </div>
       <div>
         <label className="text-white">
@@ -35,6 +72,9 @@ export default function AddProductPage() {
             name="price"
           />
         </label>
+        {state.errors.price && (
+          <p className="text-red-500">{state.errors.price}</p>
+        )}
       </div>
       <div>
         <label className="text-white">
@@ -44,14 +84,18 @@ export default function AddProductPage() {
             name="description"
           />
         </label>
+        {state.errors.description && (
+          <p className="text-red-500">{state.errors.description}</p>
+        )}
       </div>
-      {/* <button
+      <button
         type="submit"
         className="block w-full p-2 text-white bg-blue-500 rounded disabled:bg-gray-500"
+        disabled={isPending}
       >
-        Add Product
-      </button> */}
-      <Submit />
+        Submit
+      </button>
+      {/* <Submit /> */}
     </form>
   );
 }
