eb3d8a19d761fda93522d399d7da71125210e9b0
Sasikant Biswal <biswalsashikant@gmail.com>
2025-02-06 22:19:26 +0530

    Customizing Clerk Components

--- authentication-demo/src/components/navigation.tsx
+++ authentication-demo/src/components/navigation.tsx
@@ -2,7 +2,7 @@ import {
   SignInButton,
   SignOutButton,
   SignUpButton,
-  UserButton,
+  // UserButton,
   SignedIn,
   SignedOut,
 } from "@clerk/nextjs";
@@ -19,12 +19,21 @@ export const Navigation = () => {
           </div>
           <div className="flex items-center gap-4">
             <SignedOut>
-              <SignInButton mode="modal" />
+              <SignInButton>
+                <button className="px-2 py-1 text-sm border border-neutral-300 dark:border-neutral-600 dark:text-neutral-200 dark:hover:bg-neutral-700">
+                  Sign In
+                </button>
+              </SignInButton>
+              <SignUpButton>
+                <button className="px-2 py-1 text-sm border border-neutral-300 dark:border-neutral-600 dark:text-neutral-200 dark:hover:bg-neutral-700">
+                  Sign Up
+                </button>
+              </SignUpButton>
             </SignedOut>
             <SignedIn>
-              {/* <UserButton /> */}
-              <Link href={"/user-profile"}>Profile</Link>
+              <Link href="/user-profile">Profile</Link>
               <SignOutButton />
+              {/* <UserButton /> */}
             </SignedIn>
           </div>
         </div>
